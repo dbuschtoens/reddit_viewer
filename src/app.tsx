f54e6ff097da24c1f19b5f162c48173b617423ce
Tim Buschtoens <tbuschto@eclipsesource.com>
2017-11-16 16:12:31 +0100

    Extract NetworkCheckService.ts from app.ts

--- src/app.tsx
+++ src/app.tsx
@@ -1,38 +1,19 @@
 import { NavigationView, ui, AlertDialog } from 'tabris';
 import SubredditPage from './SubredditPage';
 import SubredditPresenter from './RedditService';
+import NetworkCheckService from './NetworkCheckService';
+
+new NetworkCheckService().start();
 
 export let navigationView = new NavigationView({
   left: 0, top: 0, right: 0, bottom: 0
 }).appendTo(ui.contentView);
 
 openSubredditPage('petpictures');
-document.addEventListener('online', onConnectionStateChanged, false);
 
-let connectionStateAlert = new AlertDialog({
-  title: 'wifi connection lost',
-  message: 'You have lost connection to your wifi network. Do you wish to continue browsing using mobile connection?',
-  buttons: {
-    ok: 'yes',
-    cancel: 'no'
-  }
-}).on({
-  closeCancel: () => navigationView.pages().dispose()
-});
-
-function openSubredditPage(subreddit: string) {
+export function openSubredditPage(subreddit: string) {
   let subredditPage = new SubredditPage();
   let subredditPresenter = new SubredditPresenter(subreddit);
   subredditPresenter.bind(subredditPage);
   subredditPage.appendTo(navigationView);
-}
-
-function onConnectionStateChanged() {
-  if (navigator.connection.type === Connection.WIFI) {
-    if (navigationView.pages().length === 0) {
-      openSubredditPage('petpictures');
-    }
-  } else {
-    connectionStateAlert.open();
-  }
 }
